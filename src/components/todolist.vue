439ee6b6a4949749f6afbd25f0650552d2f1bce6
kekeing <851575436@qq.com>
2019-09-04 21:25:35 +0800

    增加header

--- src/components/todolist.vue
+++ src/components/todolist.vue
@@ -3,11 +3,15 @@
         <span>todolist</span>
         <br>
         <router-link to="/index">index</router-link>
-
+        <Header/>
     </div>
 </template>
 <script>
+    import Header from './header.vue'
     export default {
+        components: {
+          Header,
+        },
         data: function(){
             return {
             }
